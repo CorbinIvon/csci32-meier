aea53f06617a413c2eb42db46575022372ed035b
Corbin M <corbinivon@gmail.com>
2024-09-06 00:27:51 -0700

    [INIT] Creates a default plage layout. - Takes children as props to enable easy to make pages.

--- src/components/client/PageLayout.tsx
+++ src/components/client/PageLayout.tsx
@@ -1,13 +1,17 @@
 // csci32-meier/src/components/client/PageLayout.tsx
 'use client' // Mark this as a Client Component
 
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, ReactNode } from 'react'
 import CollapsibleMenu from '@/components/client/navigation/CollapsibleMenu'
 import Avatar from '@/components/client/data_display/Avatar'
 // Import the JSON data
 import resourceCatalogue from '@/app/data/resource-catalogue.json'
 
-export default function HomePage() {
+interface PageLayoutProps {
+  children: ReactNode
+}
+
+const PageLayout: React.FC<PageLayoutProps> = ({ children }) => {
   const [baseUrl, setBaseUrl] = useState<string | null>(null)
 
   // Set base URL on client side
@@ -87,7 +91,7 @@ export default function HomePage() {
                 className="w-full p-2 pr-16 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
               />
               <span className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-400">Ctrl + K</span>
-            </div>{' '}
+            </div>
           </div>
 
           {/* Collapsible Menu in Aside */}
@@ -97,38 +101,7 @@ export default function HomePage() {
         </div>
 
         {/* Main content on the right */}
-        <main className="flex-1 p-6">
-          <h2 className="text-2xl font-semibold">Welcome to Corbin's Online Resource Center</h2>
-          <section className="Introduction m-4">
-            <h3 className="text-xl font-semibold">Introduction</h3>
-            <p className="mb-4">
-              For as long as I can remember, I've had a vision of creating a method to catalogue all of my thoughts and
-              things. From personal files, notes, and ideas to external resources. It was apparent that there were
-              plenty of tools available online, but nothing that fit my specific needs. It became clear that I needed a
-              custom and simple solution to organize and access all of my digital resources. This page is that solution.
-            </p>
-            <p>This project is open source and available to everyone. Installation instructions on Github.</p>
-          </section>
-          <section className="Features m-4">
-            <h3 className="text-xl font-semibold">Features</h3>
-            <ul className="list-disc pl-6">
-              <li>Simple and easy-to-use interface</li>
-              <li>Collapsible menu for easy navigation</li>
-              <li>Infinite navigation nesting</li>
-              <li>Searchable content</li>
-              <li>Well structured database</li>
-              <li>Responsive design for all device types</li>
-            </ul>
-          </section>
-          <section className="HowToUse m-4">
-            <h3 className="text-xl font-semibold">How To Use</h3>
-            <p>
-              Simply click through the navigation on the left of the screen. For mobile users, the navigation will be
-              located from the top-left tab. Click on the links to view the content. If you have any questions or
-              feedback, please feel free to reach out to me through github. Enjoy!
-            </p>
-          </section>
-        </main>
+        <main className="flex-1 p-6">{children}</main>
       </div>
 
       {/* Footer Section */}
@@ -138,3 +111,5 @@ export default function HomePage() {
     </div>
   )
 }
+
+export default PageLayout
