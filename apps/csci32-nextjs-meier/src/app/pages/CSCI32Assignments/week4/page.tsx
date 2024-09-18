8f0d5f799e3e6e3ad341a328081421243900a97c
Corbin M <corbinivon@gmail.com>
2024-09-17 22:03:01 -0700

    Adds gif to make the page temporarily better.

--- apps/csci32-nextjs-meier/src/app/pages/CSCI32Assignments/week4/page.tsx
+++ apps/csci32-nextjs-meier/src/app/pages/CSCI32Assignments/week4/page.tsx
@@ -90,6 +90,13 @@ const Csci32AssignmentWeek4 = () => {
         <br />
         <h3 className="text-xl font-semibold">Random Number Guesser</h3>
         <p>UNDER CONSTRUCTION</p>
+        <iframe
+          src="https://giphy.com/embed/6qdKZFhT0VBm0"
+          width="480"
+          height="360"
+          className="rounded-lg shadow-md"
+          allowFullScreen
+        ></iframe>
       </section>
     </PageLayout>
   )
