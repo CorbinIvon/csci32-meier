13d5aad7266833e47cdda7fc06c6d9dad21b188f
Corbin M <corbinivon@gmail.com>
2024-09-17 22:06:45 -0700

    Removes fun gif.

--- apps/csci32-nextjs-meier/src/app/pages/CSCI32Assignments/week4/page.tsx
+++ apps/csci32-nextjs-meier/src/app/pages/CSCI32Assignments/week4/page.tsx
@@ -90,13 +90,6 @@ const Csci32AssignmentWeek4 = () => {
         <br />
         <h3 className="text-xl font-semibold">Random Number Guesser</h3>
         <p>UNDER CONSTRUCTION</p>
-        <iframe
-          src="https://giphy.com/embed/6qdKZFhT0VBm0"
-          width="480"
-          height="360"
-          className="rounded-lg shadow-md"
-          allowFullScreen
-        ></iframe>
       </section>
     </PageLayout>
   )
