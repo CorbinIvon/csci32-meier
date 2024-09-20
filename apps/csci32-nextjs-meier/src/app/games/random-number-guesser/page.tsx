3da7b2798f28ddcdb147a6e595cddeee40fffe24
Corbin M <corbinivon@gmail.com>
2024-09-19 20:47:22 -0700

    [UPDATE] Makes the game window prettier

--- apps/csci32-nextjs-meier/src/app/games/random-number-guesser/page.tsx
+++ apps/csci32-nextjs-meier/src/app/games/random-number-guesser/page.tsx
@@ -94,134 +94,140 @@ export default function RandomNumberGuesserGame() {
     const blue = Math.round(255 * relativeDistance)
     return `rgb(${red}, 0, ${blue})`
   }
-
+  const gameWindow = `min-h-screen flex justify-center items-center bg-gray-200`
   return (
-    <PageLayout>
-      <h2 className="text-2xl font-semibold">Random Number Guesser</h2>
-
-      {/* Render welcome, config, game, or result screen based on currentScreen */}
-      {currentScreen === 'welcome' && (
-        <section className="welcome-screen">
-          <p>Welcome to the Random Number Guesser Game!</p>
-          <Button size={Size.MEDIUM} variant={Variant.PRIMARY} onClick={() => setCurrentScreen('config')}>
-            Start Game
-          </Button>
-        </section>
-      )}
-
-      {currentScreen === 'config' && (
-        <section className="config-screen">
-          <p>Configure your game settings:</p>
-          <div>
-            <label>Smallest Number:</label>
-            <Input type="number" value={minNumber} onChange={(e) => setMinNumber(Number(e.target.value))} />
-          </div>
-          <div>
-            <label>Largest Number:</label>
-            <Input type="number" value={maxNumber} onChange={(e) => setMaxNumber(Number(e.target.value))} />
-          </div>
-          <div>
-            <label>Max Tries:</label>
-            <Input type="number" value={maxTries} onChange={(e) => setMaxTries(Number(e.target.value))} />
-          </div>
-          <br />
-          <h3 className="mb-2 text-2xl font-extrabold leading-none tracking-tight text-gray-600">More Settings!</h3>
-          <div>
-            <Input
-              type="checkbox"
-              value={showGuessSuggestions}
-              onChange={(e) => setShowGuessSuggestions(e.target.checked)}
-              className="mr-2"
-            />
-            <label>Show Guess Suggestions</label>
-          </div>
-          <div>
-            <Input
-              type="checkbox"
-              value={showPreviousGuesses}
-              onChange={(e) => setShowPreviousGuesses(e.target.checked)}
-              className="mr-2"
-            />
-            <label>Show Previous Guesses</label>
-          </div>
-          <div>
-            <Input
-              type="checkbox"
-              value={hotAndCold}
-              onChange={(e) => setHotAndCold(e.target.checked)}
-              className="mr-2 ml-6"
-              disabled={!showPreviousGuesses}
-            />
-            <label>Enable Hot and Cold indicators</label>
-          </div>
-          <br />
-          <Button size={Size.MEDIUM} variant={Variant.PRIMARY} onClick={startGame}>
-            Start Game
-          </Button>
-        </section>
-      )}
-
-      {currentScreen === 'game' && (
-        <section className="game-window">
-          <p>{message}</p>
-          <p>Guesses Left: {guessesLeft}</p>
-
-          {/* Form for input and submit */}
-          <form onSubmit={handleGuess}>
-            <Input
-              type="number"
-              value={userGuess}
-              onChange={(e) => setUserGuess(Number(e.target.value))}
-              placeholder="Enter your guess"
-            />
-
-            {/* Button to submit the form */}
-            <Button size={Size.MEDIUM} variant={Variant.PRIMARY}>
-              Submit Guess
-            </Button>
-          </form>
-
-          {/* Show previous guesses if the option is enabled */}
-          {showPreviousGuesses && previousGuesses.length > 0 && (
-            <section className="previous-guesses">
-              <h3>Previous Guesses</h3>
-              <ul>
-                {previousGuesses.map(({ guess, result }, index) => (
-                  <li
-                    key={index}
-                    style={{
-                      backgroundColor: hotAndCold ? getColorForGuess(guess) : 'transparent',
-                    }}
-                  >
-                    {guess} was {result}
-                  </li>
-                ))}
-              </ul>
+    <PageLayout className="w-full">
+      <div className={gameWindow} style={{ minHeight: `calc(100vh - 130px)` }}>
+        <div className="bg-white p-8 rounded shadow-lg">
+          <h2 className="text-2xl font-semibold">Random Number Guesser</h2>
+          {/* Render welcome, config, game, or result screen based on currentScreen */}
+          {currentScreen === 'welcome' && (
+            <section className="welcome-screen">
+              <p>Welcome to the Random Number Guesser Game!</p>
+              <Button size={Size.MEDIUM} variant={Variant.PRIMARY} onClick={() => setCurrentScreen('config')}>
+                Start Game
+              </Button>
             </section>
           )}
 
-          {/* Button to start a new game */}
-          <Button size={Size.MEDIUM} variant={Variant.PRIMARY} onClick={() => setCurrentScreen('config')}>
-            New Game
-          </Button>
-        </section>
-      )}
-
-      {currentScreen === 'result' && (
-        <section className="result-screen">
-          {gameResult === 'win' ? (
-            <p>🎉 You Win! Congratulations! 🎉</p>
-          ) : (
-            <>
-              <p>😢 Game Over! Better luck next time! 😢</p>
-              <p>The correct number was: {targetNumber}</p>
-            </>
+          {currentScreen === 'config' && (
+            <section className="config-screen">
+              <p>Configure your game settings:</p>
+              <div>
+                <label>Smallest Number:</label>
+                <br />
+                <Input type="number" value={minNumber} onChange={(e) => setMinNumber(Number(e.target.value))} />
+              </div>
+              <div>
+                <label>Largest Number:</label>
+                <br />
+                <Input type="number" value={maxNumber} onChange={(e) => setMaxNumber(Number(e.target.value))} />
+              </div>
+              <div>
+                <label>Max Tries:</label>
+                <br />
+                <Input type="number" value={maxTries} onChange={(e) => setMaxTries(Number(e.target.value))} />
+              </div>
+              <br />
+              <h3 className="mb-2 text-2xl font-extrabold leading-none tracking-tight text-gray-600">More Settings!</h3>
+              <div>
+                <Input
+                  type="checkbox"
+                  value={showGuessSuggestions}
+                  onChange={(e) => setShowGuessSuggestions(e.target.checked)}
+                  className="mr-2"
+                />
+                <label>Show Guess Suggestions</label>
+              </div>
+              <div>
+                <Input
+                  type="checkbox"
+                  value={showPreviousGuesses}
+                  onChange={(e) => setShowPreviousGuesses(e.target.checked)}
+                  className="mr-2"
+                />
+                <label>Show Previous Guesses</label>
+              </div>
+              <div>
+                <Input
+                  type="checkbox"
+                  value={hotAndCold}
+                  onChange={(e) => setHotAndCold(e.target.checked)}
+                  className="mr-2 ml-6"
+                  disabled={!showPreviousGuesses}
+                />
+                <label>Enable Hot and Cold indicators</label>
+              </div>
+              <br />
+              <Button size={Size.MEDIUM} variant={Variant.PRIMARY} onClick={startGame}>
+                Start Game
+              </Button>
+            </section>
           )}
-          <Button size={Size.MEDIUM} variant={Variant.PRIMARY} onClick={() => setCurrentScreen('config')}>
-            Play Again
-          </Button>
-        </section>
-      )}
+
+          {currentScreen === 'game' && (
+            <section className="game-window">
+              <p>{message}</p>
+              <p>Guesses Left: {guessesLeft}</p>
+
+              {/* Form for input and submit */}
+              <form onSubmit={handleGuess}>
+                <Input
+                  type="number"
+                  value={userGuess}
+                  onChange={(e) => setUserGuess(Number(e.target.value))}
+                  placeholder="Enter your guess"
+                />
+
+                {/* Button to submit the form */}
+                <Button size={Size.MEDIUM} variant={Variant.PRIMARY}>
+                  Submit Guess
+                </Button>
+              </form>
+
+              {/* Show previous guesses if the option is enabled */}
+              {showPreviousGuesses && previousGuesses.length > 0 && (
+                <section className="previous-guesses">
+                  <h3>Previous Guesses</h3>
+                  <ul>
+                    {previousGuesses.map(({ guess, result }, index) => (
+                      <li
+                        key={index}
+                        style={{
+                          backgroundColor: hotAndCold ? getColorForGuess(guess) : 'transparent',
+                        }}
+                      >
+                        {guess} was {result}
+                      </li>
+                    ))}
+                  </ul>
+                </section>
+              )}
+
+              {/* Button to start a new game */}
+              <Button size={Size.MEDIUM} variant={Variant.PRIMARY} onClick={() => setCurrentScreen('config')}>
+                New Game
+              </Button>
+            </section>
+          )}
+
+          {currentScreen === 'result' && (
+            <section className="result-screen">
+              {gameResult === 'win' ? (
+                <p>🎉 You Win! Congratulations! 🎉</p>
+              ) : (
+                <>
+                  <p>😢 Game Over! Better luck next time! 😢</p>
+                  <p>The correct number was: {targetNumber}</p>
+                </>
+              )}
+              <Button size={Size.MEDIUM} variant={Variant.PRIMARY} onClick={() => setCurrentScreen('config')}>
+                Play Again
+              </Button>
+            </section>
+          )}
+        </div>
+      </div>
     </PageLayout>
   )
 }
