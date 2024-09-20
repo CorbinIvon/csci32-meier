6d2c9acf6a6ce79311bc70610b8d2fb40db431f9
Corbin M <corbinivon@gmail.com>
2024-09-19 20:12:15 -0700

    [UPDATE] Updates the button to handle checkboxes.

--- packages/ui/src/input.tsx
+++ packages/ui/src/input.tsx
@@ -19,10 +19,11 @@ interface InputProps {
   type?: HTMLInputTypeAttribute
   defaultValue?: any
   value?: any
-  setValue?: (newValue: string) => void
+  setValue?: (newValue: string | boolean) => void
   onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void
   name?: string
   id?: string
+  disabled?: boolean
 }
 
 export const Input = ({
@@ -37,13 +38,23 @@ export const Input = ({
   onChange,
   name,
   id,
+  disabled = false,
 }: InputProps) => {
-  const customClassName = `${getCommonStyles} ${getInputSizeStyles(size)} ${getVariantBackgroundStyles(variant)} ${getVariantOutlineStyles(variant)} ${getVariantBorderStyles(variant)} ${getVariantInputTextStyles(variant)} ${className}`
+  const checkboxStyles =
+    type === 'checkbox' ? ' w-4 h-4 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 align-middle' : ''
+  const customClassName = `${getCommonStyles} ${getInputSizeStyles(size)} ${getVariantBackgroundStyles(variant)} ${getVariantOutlineStyles(variant)} ${getVariantBorderStyles(variant)} ${getVariantInputTextStyles(variant)}${checkboxStyles} ${className}`
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    if (setValue) {
-      setValue(e.target.value)
+    if (type === 'checkbox') {
+      if (setValue) {
+        setValue(e.target.checked)
+      }
+    } else {
+      if (setValue) {
+        setValue(e.target.value)
+      }
     }
+
     if (onChange) {
       onChange(e)
     }
@@ -55,10 +66,12 @@ export const Input = ({
       placeholder={placeholder}
       className={customClassName}
       defaultValue={defaultValue}
-      value={value}
+      value={type === 'checkbox' ? undefined : value}
+      checked={type === 'checkbox' ? !!value : undefined}
       onChange={handleChange}
       name={name}
       id={id}
+      disabled={disabled}
     />
   )
 }
