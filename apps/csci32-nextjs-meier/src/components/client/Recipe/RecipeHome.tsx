caeaf64c4a9312acd8077bc81c182190d85f60a3
Corbin M <corbinivon@gmail.com>
2024-11-02 17:16:27 -0700

    Checkpoint 6 Complete

---
+++ apps/csci32-nextjs-meier/src/components/client/Recipe/RecipeHome.tsx
@@ -0,0 +1,37 @@
+import { RecipeForm } from './RecipeForm'
+import { RecipeContext } from '../../../app/pages/CSCI32Assignments/recipestacker/context/RecipeContext'
+import { Wrapper } from '@package/ui/wrapper'
+import { Flex } from '@package/ui/flex'
+import { useContext } from 'react'
+import { Button } from '@package/ui/button'
+import { Variant } from '@package/ui/variant'
+import { Header } from '@package/ui/header'
+
+export default function RecipeHome() {
+  const { showRecipeForm, setShowRecipeForm } = useContext(RecipeContext)
+  return (
+    <Wrapper>
+      <Flex className=" items-center w-full justify-between">
+        <Header variant="h1">Welcome to RecipeStacker</Header>
+        <Button
+          variant={Variant.TERTIARY}
+          onClick={() => {
+            setShowRecipeForm(!showRecipeForm)
+          }}
+        >
+          {showRecipeForm ? 'Search Recipes' : 'Create Recipe'}
+        </Button>
+      </Flex>
+
+      <Flex className="flex-col gap-y-8 mt-8">
+        {showRecipeForm ? (
+          <RecipeForm />
+        ) : (
+          <>
+            <div>🚧 Searching recipes under construction 🚧</div>
+          </>
+        )}
+      </Flex>
+    </Wrapper>
+  )
+}
